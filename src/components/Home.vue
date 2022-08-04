07e4e1afc75832d0ee3883ef5442425992f90dec
Francisco Ruiz <fruz.rc@gmail.com>
2022-08-04 15:08:38 -0400

    remove line when touch the chart

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -48,37 +48,93 @@ export default {
           title: "Movimiento",
           description: "Deposito de salario",
           amount: 100,
+          time: new Date("08-04-2022"),
         },
         {
           id: 2,
           title: "Movimiento 1",
           description: "Deposito de honorarios",
-          amount: 500,
+          amount: 200,
+          time: new Date("08-04-2022"),
         },
         {
           id: 3,
           title: "Movimiento 3",
           description: "Comida",
-          amount: 600,
+          amount: 500,
+          time: new Date("08-04-2022"),
         },
         {
           id: 4,
           title: "Movimiento 4",
           description: "Colegiatura",
-          amount: -100,
+          amount: 200,
+          time: new Date("08-04-2022"),
         },
         {
           id: 5,
           title: "Movimiento 5",
           description: "Reparación equipo",
-          amount: 200,
+          amount: -400,
+          time: new Date("08-04-2022"),
+        },
+        {
+          id: 6,
+          title: "Movimiento 6",
+          description: "Reparación equipo",
+          amount: -600,
+          time: new Date("08-04-2022"),
+        },
+        {
+          id: 7,
+          title: "Movimiento 7",
+          description: "Reparación equipo",
+          amount: -300,
+          time: new Date("08-04-2022"),
+        },
+        {
+          id: 8,
+          title: "Movimiento 8",
+          description: "Reparación equipo",
+          amount: 0,
+          time: new Date("08-04-2022"),
+        },
+        {
+          id: 9,
+          title: "Movimiento 9",
+          description: "Reparación equipo",
+          amount: 300,
+          time: new Date("07-04-2022"),
+        },
+        {
+          id: 10,
+          title: "Movimiento 10",
+          description: "Reparación equipo",
+          amount: 500,
+          time: new Date("07-04-2022"),
         },
       ],
     };
   },
   computed: {
     amounts() {
-      return this.movements.map((movement) => movement.amount);
+      const lastDays = this.movements
+        .filter((mov) => {
+          const today = new Date();
+          const oldDate = today.setDate(today.getDate() - 30);
+          return mov.time >= oldDate;
+        })
+        .map((movement) => movement.amount);
+
+      console.log(lastDays);
+
+      return lastDays.map((movement, idx) => {
+        const lastMovements = lastDays.slice(0, idx);
+
+        return lastMovements.reduce((sum, movement) => {
+          return sum + movement;
+        }, 0);
+      });
     },
   },
 };
